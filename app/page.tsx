650e51d01f84a69a6e0c1da38198b3edc3eb6267
ahsanrkabir <ahsanrifat001@gamil.com>
2024-12-19 01:19:36 +0600

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-white font-black">Welcome to CodeOverflow</h1>
+    <h1 className="text-3xl font-black text-white bg-green-100">
+      Welcome to CodeOverflow
+    </h1>
   );
-}
+};
 
 export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,36 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "standard", "plugin:tailwindcss/recommended", "prettier"),
+  ...compat.plugins("import"),
+  ...compat.rules({
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object"
+        ],
+        "newlines-between": "always",
+        "pathGroups": [
+          {
+            "pattern": "@app/**",
+            "group": "external",
+            "position": "after"
+          }
+        ],
+        "pathGroupsExcludedImportTypes": ["builtin"],
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
+  }),
 ];
 
 export default eslintConfig;
