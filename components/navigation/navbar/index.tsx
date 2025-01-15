85f69db68d9b4516c173159d6e60aacc40074467
ahsanrkabir <ahsanrifat001@gamil.com>
2025-01-16 05:44:37 +0600

    Setup Social Auth Form, NextAuth, OAuth with Google & Github Providers

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 import Theme from "@/components/navigation/navbar/Theme";
+import ROUTES from "@/constants/routes";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href={ROUTES.HOME} className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
           alt="CodeOverFlow Logo"
